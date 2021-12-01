50544919851b5c9e74ee770a5ffbc8e9d346bc22
themrspartan2 <nathanfarrar22@gmail.com>
2021-12-01 13:42:43 -0500

    some stuff 2

--- Client.cpp
+++ Client.cpp
@@ -1,4 +1,17 @@
 //Nathan Farrar
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <iostream>
+#include <thread>
+#include <list>
 #include <iostream>
 
 using namespace std;
--- Server.cpp
+++ Server.cpp
@@ -1,10 +1,29 @@
 //Nathan Farrar
+#define MYSQLPP_MYSQL_HEADERS_BURIED
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <iostream>
+#include <thread>
+#include <list>
+#include <mysql++/mysql++.h>
+#include <string>
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    // Connect to database with: database, server, userID, password
+    //string username, password;
+    mysqlpp::Connection myDB("cse278", "localhost", "cse278", "S3rul3z");
+
     return 0;
 }
