3f4b2dd216ec21f5346a012545ebb41b40038220
themrspartan2 <nathanfarrar22@gmail.com>
2021-12-01 14:30:35 -0500

    added server code

--- Client.cpp
+++ Client.cpp
@@ -14,10 +14,103 @@
 #include <list>
 #include <iostream>
 
+#define MYPORT 7000
+#define BUFFER_SIZE 1024
+
 using namespace std;
 
+void connect()
+{
+}
 int main(int argc, char const *argv[])
 {
-    /* code */
+    //Get your username and password
+    string username, password;
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    cin >> password;
+
+    int sock_cli;
+    fd_set rfds;
+    struct timeval tv;
+    int retval, maxfd;
+
+    //define sockfd
+    sock_cli = socket(AF_INET, SOCK_STREAM, 0);
+    //define sockaddr_in
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(MYPORT);                 //server port
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); //server ip
+
+    //connect to the server
+    //0 = success, -1 = error
+    if (connect(sock_cli, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("connect");
+        exit(1);
+    }
+
+    while (1)
+    {
+        //clear the collection of readable file descriptors
+        FD_ZERO(&rfds);
+
+        //add standard input file descriptors to the collection
+        FD_SET(0, &rfds);
+        maxfd = 0;
+
+        //add the currently connected file descriptor to the collection
+        FD_SET(sock_cli, &rfds);
+
+        //find the largest file descriptor in the file descriptor set
+        if (maxfd < sock_cli)
+        {
+            maxfd = sock_cli;
+        }
+
+        //setting timeout time
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+
+        //waiting for message
+        retval = select(maxfd + 1, &rfds, NULL, NULL, &tv);
+        if (retval == -1)
+        {
+            printf("Select error, exiting client program.\n");
+            break;
+        }
+        else if (retval == 0)
+        {
+            //waiting for input from the client
+            continue;
+        }
+        else
+        {
+            //The server sent a message.
+            if (FD_ISSET(sock_cli, &rfds))
+            {
+                char recvbuf[BUFFER_SIZE];
+                int len;
+                len = recv(sock_cli, recvbuf, sizeof(recvbuf), 0);
+                printf("%s", recvbuf);
+                memset(recvbuf, 0, sizeof(recvbuf));
+            }
+            //When the user enters the information, he begins to process the information and send it.
+            if (FD_ISSET(0, &rfds))
+            {
+                char sendbuf[BUFFER_SIZE];
+                fgets(sendbuf, sizeof(sendbuf), stdin);
+                //cout << sendbuf;
+                send(sock_cli, sendbuf, strlen(sendbuf), 0); //Send out
+                memset(sendbuf, 0, sizeof(sendbuf));
+            }
+        }
+    }
+
+    close(sock_cli);
+
     return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -14,16 +14,123 @@
 #include <thread>
 #include <list>
 #include <mysql++/mysql++.h>
-#include <string>
-#include <iostream>
+
+#define MYPORT 7000
+#define BUFFER_SIZE 1024
+#define IP "127.0.0.1"
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    // Connect to database with: database, server, userID, password
-    //string username, password;
-    mysqlpp::Connection myDB("cse278", "localhost", "cse278", "S3rul3z");
+int s;
+struct sockaddr_in servaddr;
+socklen_t len;
+list<int> li;
 
+void getConnection()
+{
+    while (1)
+    {
+        int conn = accept(s, (struct sockaddr *)&servaddr, &len);
+        li.push_back(conn);
+        printf("%d\n", conn);
+    }
+}
+
+void getData()
+{
+    //server checks for message every 2 seconds
+    struct timeval tv;
+    tv.tv_sec = 2;
+    tv.tv_usec = 0;
+
+    while (1)
+    {
+        list<int>::iterator it;
+        for (it = li.begin(); it != li.end(); ++it)
+        {
+            fd_set rfds;
+            FD_ZERO(&rfds);
+            int maxfd = 0;
+            int retval = 0;
+            FD_SET(*it, &rfds);
+
+            if (maxfd < *it)
+            {
+                maxfd = *it;
+            }
+            retval = select(maxfd + 1, &rfds, NULL, NULL, &tv);
+            if (retval == -1)
+            {
+                printf("Socket error\n");
+            }
+            else if (retval == 0)
+            {
+                //No messages were recieved from this connection
+            }
+            else
+            {
+                char buf[BUFFER_SIZE];
+                memset(buf, 0, sizeof(buf));
+                int len = recv(*it, buf, sizeof(buf), 0);
+                printf("%s", buf);
+                //now send it to everyone but the sender
+                //send(*it, buf, sizeof(buf), 0);
+            }
+        }
+        sleep(1);
+    }
+}
+
+void sendMess()
+{
+    while (1)
+    {
+        char buf[BUFFER_SIZE];
+        fgets(buf, sizeof(buf), stdin);
+        list<int>::iterator it;
+        for (it = li.begin(); it != li.end(); ++it)
+        {
+            send(*it, buf, sizeof(buf), 0);
+        }
+    }
+}
+
+int main()
+{
+    //Create the socket - ipv4 internet socket, TCP
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(MYPORT);
+    servaddr.sin_addr.s_addr = inet_addr(IP);
+
+    //bind the socket with error checking
+    if (bind(s, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
+    {
+        perror("bind");
+        exit(1);
+    }
+    if (listen(s, 20) == -1)
+    {
+        perror("listen");
+        exit(1);
+    }
+    len = sizeof(servaddr);
+
+    //thread : while ==>> accpet
+    thread t(getConnection);
+    t.detach();
+
+    //thread : input ==>> send
+    thread t1(sendMess);
+    t1.detach();
+
+    //thread : recv ==>> show
+    thread t2(getData);
+    t2.detach();
+
+    while (1)
+    {
+    }
     return 0;
 }
